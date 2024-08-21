d5d5d4795c8ed0b2327e777f1ce4c9dcd366f122
Jacek Pasierb <jack_byk@o2.pl>
2024-08-21 20:01:21 +0200

    Feature/About - change my description

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // src/app/about/page.tsx
-import React from 'react';
-import styles from './page.module.css';
-import { poppins } from './ui/fonts';
+import React from "react";
+import styles from "./page.module.css";
+import { poppins } from "./ui/fonts";
 
 const AboutPage: React.FC = () => {
   return (
@@ -14,36 +14,37 @@ const AboutPage: React.FC = () => {
         <div className={styles.bio}>
           <h2 className={poppins.className}>Witaj!</h2>
           <p>
-            Cześć! Nazywam się Jacek Pasierb i jestem Full Stack Developerem z pasją do tworzenia innowacyjnych rozwiązań
-            w zakresie technologii webowych. Specjalizuję się w <strong>React</strong> i <strong>Node.js</strong>, ale
-            mam również doświadczenie z innymi technologiami jak <strong>MongoDB</strong> i <strong>TypeScript</strong>.
+            Cześć! Nazywam się Jacek Pasierb i jestem Full Stack Developerem z
+            pasją do tworzenia innowacyjnych rozwiązań w zakresie technologii
+            webowych. Specjalizuję się w <strong>React</strong> i{" "}
+            <strong>Node.js</strong>, mam doświadczenie z bazami danych{" "}
+            <strong>MongoDB</strong> i <strong>Postgres</strong> oraz
+            technologiami <strong>TypeScript</strong>.
           </p>
           <p>
-            Od kilku lat pracuję w branży IT, realizując projekty, które wymagają zarówno kreatywności, jak i solidnego
-            podejścia do programowania. Moje umiejętności obejmują projektowanie responsywnych interfejsów użytkownika, budowanie
-            skalowalnych aplikacji webowych oraz integrację z różnymi usługami i API.
+            Obecnie poszerzam swoją wiedzę o <strong>Next.js</strong>,
+            frameworku do tworzenia aplikacji webowych. Chociaż dopiero uczę się
+            jego zaawansowanych funkcji, już teraz potrafię wykorzystać jego
+            możliwości do budowania wydajnych i nowoczesnych stron
+            internetowych.
           </p>
           <p>
-            Poza programowaniem, interesuję się <strong>technologiami</strong>, <strong>psychologią</strong>, oraz
-            <strong>podróżami</strong>. W wolnym czasie lubię czytać książki, zwiedzać nowe miejsca i uczyć się nowych
-            umiejętności.
+            Od 2 lat pracuję w branży IT, realizując projekty, które wymagają
+            zarówno kreatywności, jak i solidnego podejścia do programowania.
+            Moje umiejętności obejmują projektowanie responsywnych interfejsów
+            użytkownika, budowanie skalowalnych aplikacji webowych oraz
+            integrację z różnymi usługami i API.
+          </p>
+          <p>
+            Poza programowaniem, interesuję się <strong> technologiami </strong>
+            ,<strong> psychologią </strong> , oraz
+            <strong> podróżami </strong>. W wolnym czasie lubię czytać książki,
+            zwiedzać nowe miejsca i uczyć się nowych umiejętności.
           </p>
         </div>
-      </section>
-      <section className={styles.skills}>
-        <h2 className={poppins.className}>Moje Umiejętności</h2>
-        <ul>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>MongoDB</li>
-          <li>TypeScript</li>
-          <li>CSS & SCSS</li>
-          <li>APIs</li>
-        </ul>
       </section>
     </main>
   );
 };
 
 export default AboutPage;
-
